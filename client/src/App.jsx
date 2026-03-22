1d253c3c808f7c91ee38d4217a0d0422b3acc1f3
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-03-22 13:48:35 +0530

    Created home page route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div>App</div>;
+  const isSllerPath = useLocation().pathname.includes("seller");
+  return (
+    <div>
+      {isSllerPath ? null : <Navbar />}
+      <div
+        className={`${isSllerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
