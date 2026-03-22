4d15fd8d0e1dc245823abcd007b64cff47107d00
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-03-22 23:09:22 +0530

    Mounted categoreis,best seller and bottom banner sections

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import MainBanner from "../components/MainBanner";
+import Catogories from "../components/Catogories";
+import BestSeller from "../components/BestSeller";
+import BottomBanner from "../components/BottomBanner";
 
 const Home = () => {
   return (
     <div className="mt-10">
       <MainBanner />
+      <Catogories />
+      <BestSeller />
+      <BottomBanner />
     </div>
   );
 };
