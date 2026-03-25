2fd97ee5dfb13e4175ec9081fd17c3107f27cf49
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-03-25 10:08:48 +0530

    created user auth controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -71,3 +71,15 @@ export const login = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+//Check auth
+export const isAuth = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const user = await User.findById(userId).select("-password");
+    return res.json({ success: true, user });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
