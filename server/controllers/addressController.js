4d814e81901467cfa4bf15ebe4c80c22fd32cf7b
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-03-26 15:03:00 +0530

    Created address controller

---
+++ server/controllers/addressController.js
@@ -0,0 +1,24 @@
+import Address from "../models/Address.js";
+//Add address
+export const addAddress = async (req, res) => {
+  try {
+    const { address } = req.body;
+    const { userId } = req;
+    await Address.create({ ...address, userId });
+    res.json({ success: true, message: "Address added successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+//Get address
+export const getAddress = async (req, res) => {
+  try {
+    const { userId } = req;
+    const addresses = await Address.find({ userId });
+    res.json({ success: true, addresses });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
