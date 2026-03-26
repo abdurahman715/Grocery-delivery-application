c64097502ddc15736d7b7ce950aa9b829e9ed408
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-03-26 16:21:47 +0530

    Added backend URL

--- server/server.js
+++ server/server.js
@@ -15,7 +15,10 @@ const port = process.env.PORT || 4000;
 await connectDb();
 await connectCloudinary();
 //Allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://grocery-delivery-application-blush.vercel.app",
+];
 //Middleware configuration
 app.use(express.json());
 app.use(cookieParser());
