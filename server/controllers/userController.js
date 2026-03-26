1cc96ef519917187227511ff7643fb1079b29dae
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-03-26 14:50:52 +0530

    Added logout functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -20,7 +20,8 @@ export const register = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      securet: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.json({
@@ -59,7 +60,8 @@ export const login = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      securet: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.json({
@@ -75,9 +77,24 @@ export const login = async (req, res) => {
 //Check auth
 export const isAuth = async (req, res) => {
   try {
-    const { userId } = req.body;
+    const { userId } = req;
     const user = await User.findById(userId).select("-password");
     return res.json({ success: true, user });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//Logout
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+    return res.json({ success: true, message: "Logged out" });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
