303dfc183633fd5c56ee27da006b6899f6836e2b
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-03-26 16:00:13 +0530

    Added extensions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.jsx";
 import { Route, Router, Routes, useLocation } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 import { Toaster } from "react-hot-toast";
-import Footer from "./components/Footer";
-import { useAppContext } from "./context/AppContext";
-import Login from "./components/Login";
-import AllProducts from "./pages/AllProducts";
-import ProductCategory from "./pages/ProductCategory";
-import ProductDetails from "./pages/ProductDetails";
-import Cart from "./pages/Cart";
-import AddAddress from "./pages/AddAddress";
-import MyOrders from "./pages/MyOrders";
-import SellerLogin from "./components/Seller/SellerLogin";
-import SellerLayout from "./pages/seller/SellerLayout";
-import AddProduct from "./pages/seller/AddProduct";
-import ProductList from "./pages/seller/ProductList";
-import Orders from "./pages/seller/Orders";
+import Footer from "./components/Footer.jsx";
+import { useAppContext } from "./context/AppContext.jsx";
+import Login from "./components/Login.jsx";
+import AllProducts from "./pages/AllProducts.jsx";
+import ProductCategory from "./pages/ProductCategory.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
+import Cart from "./pages/Cart.jsx";
+import AddAddress from "./pages/AddAddress.jsx";
+import MyOrders from "./pages/MyOrders.jsx";
+import SellerLayout from "./pages/seller/SellerLayout.jsx";
+import AddProduct from "./pages/seller/AddProduct.jsx";
+import ProductList from "./pages/seller/ProductList.jsx";
+import Orders from "./pages/seller/Orders.jsx";
+import SellerLogin from "./components/Seller/SellerLogin.jsx";
 
 const App = () => {
   const isSllerPath = useLocation().pathname.includes("seller");
